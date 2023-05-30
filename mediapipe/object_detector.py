4143c48eb3b2f577263574aebec1d5f720093ce1
POLaforcade <po.laforcade@gmail.com>
2023-05-30 09:57:27 +0200

    working object detector

--- mediapipe/object_detector.py
+++ mediapipe/object_detector.py
@@ -20,29 +20,29 @@ def visualize(image, detection_result) -> np.ndarray:
     Image with bounding boxes.
   """
   for detection in detection_result.detections:
-    # Draw bounding_box
-    bbox = detection.bounding_box
-    start_point = bbox.origin_x, bbox.origin_y
-    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(image, start_point, end_point, TEXT_COLOR, 3)
+    if(detection.categories[0].category_name == 'person'):
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(image, start_point, end_point, TEXT_COLOR, 3)
 
-    # Draw label and score
-    category = detection.categories[0]
-    category_name = category.category_name
-    probability = round(category.score, 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (MARGIN + bbox.origin_x,
-                     MARGIN + ROW_SIZE + bbox.origin_y)
-    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
+        # Draw label and score
+        category = detection.categories[0]
+        category_name = category.category_name
+        probability = round(category.score, 2)
+        result_text = category_name + ' (' + str(probability) + ')'
+        text_location = (MARGIN + bbox.origin_x,
+                        MARGIN + ROW_SIZE + bbox.origin_y)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
 
   return image
 
 def detect_object(file_path : str):
-    """
-    Show a video with bounding boxes around detected person
-        Args :
-            file_path : path to the RGB video that msut be computed
+    """Show a video with bounding boxes around detected person
+    Args :
+        file_path : path to the RGB video that msut be computed
     """
     # Open capture
     cap = cv2.VideoCapture(file_path)
